c426cb8204f9713283c2aadef785d425e80cd3ce
ah-0 <ah-0>
2024-03-10 11:25:21 +0300

    update treeview.py

--- src/app/treeview.py
+++ src/app/treeview.py
@@ -183,7 +183,7 @@ class FileManager(QTreeView):
             f = Path(_path) / _name
             count = 1
             while f.exists():
-                f = Path(f.parent / f"{_name}{count}")
+                f = Path(f.parent / f"{_name}{count}{f.suffix}")
                 count += 1
             f.touch()
             
